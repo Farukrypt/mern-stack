4c4aaac2a2db9f110d599b5fe7916f35c3863d66
unknown <farukrypt@gmail.com>
2025-09-16 10:53:43 +0100

    Update signup hook and add API_URL env

--- mern stack/backend/server.js
+++ mern stack/backend/server.js
@@ -1,35 +1,37 @@
 require('dotenv').config();
 
 const express = require('express');
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workout')
-const userRoutes = require('./routes/user')
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workout');
+const userRoutes = require('./routes/user');
+const cors = require('cors');
 
 //express app
 const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
-console.log(req.path, req.method)
-next()
+  console.log(req.path, req.method);
+  next();
 });
 
 //routes
-app.use('/api/workouts', workoutRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 //connect to db
-mongoose.connect(process.env.MONGO_URI)
-.then(() =>{
-//listen for reuests
-const PORT = process.env.PORT;
-app.listen(PORT, function() {
-    console.log(`Connected to db & Server started on port ${PORT}`);
-});
-
-})
-.catch((error) =>{
-    console.log(error)
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for reuests
+    const PORT = process.env.PORT;
+    app.listen(PORT, function () {
+      console.log(`Connected to db & Server started on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- mern stack/frontend/src/pages/Home.js
+++ mern stack/frontend/src/pages/Home.js
@@ -1,45 +1,46 @@
-import React, { useEffect } from 'react'
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
-import { useAuthContext }  from '../hooks/useAuthContext'                
+import React, { useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 //components
-import WorkoutDetails from '../components/WorkoutDetails'
-import WorkoutForm from '../components/WorkoutForm'
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
+
+const API_URL = process.env.REACT_APP_API_URL || '';
 
 const Home = () => {
- const {workouts, dispatch} = useWorkoutsContext()
-const {user} = useAuthContext()
+  const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch(`${API_URL}/api/workouts`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      const json = await response.json();
 
-    useEffect(() => {
-        const fetchWorkouts = async () =>{
-            const response = await fetch('/api/workouts', {
-             headers: {
-               'Authorization': `Bearer ${user.token}`
-             }
-            })
-              const json = await response.json()
+      if (response.ok) {
+        dispatch({ type: 'SET_WORKOUTS', payload: json });
+      }
+    };
 
-            if (response.ok) {
-               dispatch({type: 'SET_WORKOUTS', payload: json})
-            }
-        }
-        
-        if (user) {
-          fetchWorkouts()
-        }
-        
-    }, [dispatch, user])
+    if (user) {
+      fetchWorkouts();
+    }
+  }, [dispatch, user]);
   return (
     <div className='home'>
-    <div className='workouts'>
-        {workouts && workouts.map((workout) => (
-          <WorkoutDetails key={workout._id} workout={workout}/>
-        ))}
+      <div className='workouts'>
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
     </div>
-          <WorkoutForm />
-        </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
